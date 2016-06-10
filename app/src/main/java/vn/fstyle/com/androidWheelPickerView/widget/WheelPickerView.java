46a41c6d353c0bf6e2009cc1f41884861afa36b5
Sun <daolq@asiantech.vn>
2016-06-10 22:00:15 +0700

    finish

--- app/src/main/java/vn/fstyle/com/androidWheelPickerView/widget/WheelPickerView.java
+++ app/src/main/java/vn/fstyle/com/androidWheelPickerView/widget/WheelPickerView.java
@@ -2,8 +2,10 @@ package vn.fstyle.com.androidWheelPickerView.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -34,20 +36,28 @@ public class WheelPickerView extends FrameLayout {
 
     public WheelPickerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        setAttributeSet(context, attrs);
+    }
+
+    private void setAttributeSet(Context context, AttributeSet attrs) {
+        View parentView = LayoutInflater.from(context).inflate(R.layout.item_picker, this);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.WheelPickerView);
+        LoopView item = (LoopView) parentView.findViewById(R.id.loopView);
+        int textColor = typedArray.getColor(R.styleable.WheelPickerView_itemTextColor, getResources().getColor(android.R.color.darker_gray));
+        item.setTextColor(textColor);
+        int textColorSelected = typedArray.getColor(R.styleable.WheelPickerView_itemTextColorSelect, getResources().getColor(android.R.color.black));
+        item.setTextColorSelected(textColorSelected);
+        int textSize = typedArray.getDimensionPixelOffset(R.styleable.WheelPickerView_itemTextSize,
+                getResources().getDimensionPixelOffset(R.dimen.activity_horizontal_margin));
+        item.setTextSize(textSize);
+        wheelOptions = new WheelOptions(this);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public WheelPickerView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context);
+        setAttributeSet(context, attrs);
     }
-
-    private void init(Context context) {
-        View.inflate(context, R.layout.item_picker, this);
-        wheelOptions = new WheelOptions(this);
-    }
-
 
     public void setPicker(List<String> optionsItems) {
         wheelOptions.setPicker(optionsItems);
